73504161245fafb8e98798991132a274a86ccb40
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2022-04-07 19:46:00 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
-*main - Entry point
-Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+}
