0b2314a8f8b462f25bd46e5b0df33d87faeeffce
solomonkifle <solomonkifle21@gmail.com>
2022-04-07 09:26:00 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+include <stdio.h>
+
 /**
-    * main - Entry point
-    * Return: return 1 after program execution
-    */
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n);
-        return (0);
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
 }
