f1030ca3e77c76d73629f2ae7c30257249d11dba
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2022-04-12 18:24:09 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,16 @@
-hdhfgdf
+#include "main.h"
+/**
+ * main - entry block
+ * @voil: no argument
+ * Return: 0
+ **/
+int main(void)
+{
+  char solomon[] = "_putchar";
+  int c;
+  for (c = 0; c < 8); c++)
+  {
+    _putchar(solomon[]);
+  }
+  _putchar('\n');
+  return (0);
