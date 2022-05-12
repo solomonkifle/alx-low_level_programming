68b70c445f123b346067517161505a60b6bda7cb
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2022-05-12 19:14:00 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,12 +2,9 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * print_numbers - Function that prints numbers,
- *                 followed by a new line.
- *
+ * print_numbers - Function that prints numbers, followed by a new line.
  * @separator: This is input separator
  * @n: This is the input number
- *
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
