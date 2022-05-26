9d748c67a8e4994949e468d629a3891d424cebcb
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2022-05-26 19:42:56 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * AUthor: Yovo Koffi Vianney
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-bit_manipulation directory.
- */
-
+/*prototypes*/
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -17,4 +11,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
-#endif /* MAIN_H */
+#endif
