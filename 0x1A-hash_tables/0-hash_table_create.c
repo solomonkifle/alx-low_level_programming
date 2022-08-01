d900495d6262dff004b773300740d7b55063753c
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2022-08-01 21:00:12 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table (as name implies) of size @size
- * @size: The size of the hash table (the spaces allocated for nodes)
- * Return: pointer to the hash table on success, else, null
+ * hash_table_create - Creates a hash table.
+ *
+ * @size: The size of the array.
+ *
+ * Return: If something went wrong it returns NULL, otherwise it
+ * returns a pointer to the newely created hash table.
+ *
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	unsigned long int index = 0;
 
-	hash_table_t *hash_table;
-
-	if (size == 0)
+	if (new_table == NULL)
 		return (NULL);
 
-	hash_table = malloc(sizeof(hash_table_t));
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (hash_table == NULL)
+	if (new_table->array == NULL)
 		return (NULL);
 
-	hash_table->size = size;
-	hash_table->array = calloc(size, sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
-		return (NULL);
-	}
-	return (hash_table);
+	for (; index < size; index++)
+		new_table->array[index] = NULL;
+
+	return (new_table);
 }
